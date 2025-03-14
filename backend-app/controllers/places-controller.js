5a115d432d3cffbfa2ee457df3a00700815135f9
Taner Özer 😌 <tanerozer16@gmail.com>
2025-03-14 17:36:14 +0300

    places controllerjs added

---
+++ backend-app/controllers/places-controller.js
@@ -0,0 +1,208 @@
+const fs = require("fs");
+
+const mongoose = require("mongoose");
+const { validationResult } = require("express-validator");
+
+const Place = require("../models/place.js");
+const User = require("../models/user");
+const getCoordinatesFromAddress = require("../util/location.js");
+const { createError } = require("../util/http-error.js");
+
+const getPlaceById = async (req, res, next) => {
+  const { placeId } = req.params;
+  let place;
+
+  try {
+    place = await Place.findById(placeId).populate("creator");
+  } catch (err) {
+    return next(createError("Couldn't find a place for the given ID!", 404));
+  }
+
+  if (!place) {
+    return next(createError("Couldn't find a place for the given ID!", 404));
+  }
+
+  res.json({
+    success: true,
+    place: place.toObject({ getters: true }),
+  });
+};
+
+const getPlaces = async (req, res, next) => {
+  try {
+    const places = await Place.find();
+    res.json({
+      success: true,
+      places: places.map((place) => place.toObject({ getters: true })),
+    });
+  } catch (err) {
+    return next(createError("Error fetching places from the database!", 500));
+  }
+};
+
+const getPlacesByUserId = async (req, res, next) => {
+  const { userId } = req.params;
+  let places;
+
+  try {
+    const user = await User.findById(userId).populate("places");
+
+    places = user.places;
+  } catch (err) {
+    return next(
+      createError("Couldn't fetch places for the given user ID!", 404)
+    );
+  }
+
+  if (!places) {
+    return next(
+      createError("Couldn't fetch places for the given user ID!", 404)
+    );
+  }
+
+  res.json({
+    success: true,
+    places: places.map((place) => place.toObject({ getters: true })),
+  });
+};
+
+const createPlace = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(createError("Invalid information!", 422));
+  }
+
+  const { title, description, address } = req.body;
+  let coordinates, user;
+
+  try {
+    coordinates = await getCoordinatesFromAddress(address);
+  } catch (err) {
+    return next(
+      createError("The entered address is not a valid address!", 406)
+    );
+  }
+
+  const createdPlace = new Place({
+    title,
+    description,
+    address,
+    location: coordinates,
+    image: req.file.path,
+    creator: req.user.userId,
+  });
+
+  try {
+    user = await User.findById(req.user.userId);
+  } catch (err) {
+    return next(createError("Place creation failed!", 500));
+  }
+
+  if (!user) {
+    return next(createError("Couldn't find user for provided ID!", 404));
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    await createdPlace.save({ session });
+
+    user.places.push(createdPlace);
+    await user.save({ session });
+
+    await session.commitTransaction();
+  } catch (err) {
+    return next(createError("Couldn't write data to database!", 500));
+  }
+
+  res.status(201).json({ success: true, place: createdPlace });
+};
+
+const updatePlaceById = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(createError("Invalid information!", 422));
+  }
+
+  const { placeId } = req.params;
+  const { title, description } = req.body;
+  let place;
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    return next(createError("Couldn't find place!", 500));
+  }
+
+  if (place.creator.toString() !== req.user.userId) {
+    return next(
+      createError("You can't edit places that don't belong to you!", 401)
+    );
+  }
+
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    return next(createError("Couldn't save place!", 500));
+  }
+
+  res
+    .status(200)
+    .json({ succces: true, place: place.toObject({ getters: true }) });
+};
+
+const deletePlaceById = async (req, res, next) => {
+  const { placeId } = req.params;
+  let place;
+
+  try {
+    place = await Place.findById(placeId).populate("creator");
+  } catch (err) {
+    return next(createError("Couldn't find place for the given ID!", 500));
+  }
+
+  if (!place) {
+    return next(createError("Couldn't find place for the given ID!", 500));
+  }
+
+  if (place.creator.id !== req.user.userId) {
+    return next(
+      createError("You can't delete places that don't belong to you!", 401)
+    );
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    await place.deleteOne({ session });
+
+    place.creator.places.pull(place);
+    await place.creator.save({ session });
+
+    await session.commitTransaction();
+  } catch (err) {
+    return next(createError("Couldn't remove place!", 500));
+  }
+
+  fs.unlink(place.image, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+
+  res.status(200).json({ message: "Deleted place!" });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlaceById = updatePlaceById;
+exports.deletePlaceById = deletePlaceById;
+exports.getPlaces = getPlaces;
